03ccce47ada675c0ab45ff685c5b7b510996251c
cford14 <cford14@gmail.com>
2020-02-21 11:27:21 -0600

    Updates

---
+++ precache-manifest.e8e21e81c683ac1531e2179611afb11b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d596ddfe49b5e6f52cc2ed137cd6bd24",
+    "url": "/react_practice/index.html"
+  },
+  {
+    "revision": "f7758b7ad16724b78a16",
+    "url": "/react_practice/static/css/main.eb4c0b72.chunk.css"
+  },
+  {
+    "revision": "e5ca110051aa84c82214",
+    "url": "/react_practice/static/js/2.458ea341.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react_practice/static/js/2.458ea341.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7758b7ad16724b78a16",
+    "url": "/react_practice/static/js/main.dabe41a9.chunk.js"
+  },
+  {
+    "revision": "6d5f2b87dd4e74cae820",
+    "url": "/react_practice/static/js/runtime-main.c87b5207.js"
+  },
+  {
+    "revision": "21fa268717696481dddf99141615ea50",
+    "url": "/react_practice/static/media/bkground.21fa2687.jpg"
+  },
+  {
+    "revision": "1af616d2abaaeab59a85c3a188b1246b",
+    "url": "/react_practice/static/media/cover.1af616d2.jpg"
+  },
+  {
+    "revision": "357ab4a88993fa36062b7a205a52d12a",
+    "url": "/react_practice/static/media/p1.357ab4a8.jpg"
+  },
+  {
+    "revision": "c800bef256bbd6eb3c079682ab7369bd",
+    "url": "/react_practice/static/media/p2.c800bef2.jpg"
+  },
+  {
+    "revision": "79b223604d98da0137fdf53a0b11a8f6",
+    "url": "/react_practice/static/media/p3.79b22360.jpg"
+  },
+  {
+    "revision": "fbc918b689f0cc5e1b6151fa1b61055c",
+    "url": "/react_practice/static/media/p4.fbc918b6.jpg"
+  },
+  {
+    "revision": "41fb52025db936ba17c5356cf9206a33",
+    "url": "/react_practice/static/media/p5.41fb5202.jpg"
+  }
+]);
